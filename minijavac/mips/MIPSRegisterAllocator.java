334701f73ac7094b7a2ff62b5cec3c66e5f04c1a
Joel Griffith <joel@joel-griffith.com>
2012-12-08 17:32:05 -0500

    hook up dem allocators

--- minijavac/mips/MIPSRegisterAllocator.java
+++ minijavac/mips/MIPSRegisterAllocator.java
@@ -2,6 +2,7 @@ package minijavac.mips;
 
 import minijavac.*;
 import minijavac.ir.*;
+import minijavac.graph.*;
 
 public class MIPSRegisterAllocator {
 	private SymbolTable symbolTable;
@@ -13,6 +14,12 @@ public class MIPSRegisterAllocator {
 	}
 
 	public void allocate(IR ir) {
+		for (int i = 0; i < ir.size(); i++) {
+			MethodIR methodIR = ir.getMethodIR(i);
+			// graph color the method's ir to find optimal register use
+			RegisterAllocator regAlloc = new RegisterAllocator(methodIR);
+			regAlloc.color();
+		}
 	}
 
 	public String getReservedRegister() {
