e0b0ee211e9eeb556e0bca160cf8791564828f1f
Sean Myers <SeanMyers0608@gmail.com>
2012-12-09 16:45:21 -0500

    Added stack allocation on params. Working on formals now

--- minijavac/mips/MIPSFrame.java
+++ minijavac/mips/MIPSFrame.java
@@ -1,15 +1,29 @@
 package minijavac.mips;
 
+import java.util.*;
+
 public class MIPSFrame {
 	private String canonicalMethodName;
+    private Map<String, Integer> offset;
 	private int size;
 
 	public MIPSFrame(String method) {
 		canonicalMethodName = method;
 		// 8 bytes are necessary to store $fp and $ra
 		size = 8;
+        offset = new HashMap<String, Integer>();
 	}
 
+    public int get(String varName){
+        return offset.get(varName);
+    }
+
+    public void allocate(String varName){
+        offset.put(varName, size);
+        size += 4;
+
+
+    }
 	public void setSize(int bytes) {
 		size = bytes;
 	}
