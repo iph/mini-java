ca3a27acd87634cbe4610dd4bcf69b356aa53b59
Joel Griffith <joel@joel-griffith.com>
2012-12-09 23:55:58 -0500

    add object

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-INPUT_FILE = tests/BinarySearch.java
+INPUT_FILE = tests/milestones/Milestone8.java
 OUTPUT_FILE = Milestone1.txt
 
 TEST_FILES = easy-test.java test.java BubbleSort.java \
---
+++ tests/Object.java
@@ -0,0 +1,13 @@
+class Test {
+    public static void main(String[] args) {
+        System.out.println(new Test2().Start());
+    }
+}
+
+class Test2 {
+	int var;
+
+	public int Start() {
+		return var;
+	}
+}
